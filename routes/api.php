ce0fa0036f0dfe9292ea3d597d6917e2557d17b3
NikolaiProgramist <nikluk2005@mail.ru>
2025-05-31 15:28:10 +0300

    Add authorization for /api/v1/cars routes
    Add AuthController.php with register, login and logout actions.
    Add LoginUserRequest.php and StoreUserRequest.php requests for the AuthController.php.
    Update User.php model: add HasApiTokens trait and update $hidden property.
    Update config/sanctum.php configure file.
    Update routes and add auth:sanctum middleware to the api.php.
    Add Schedule::command for remove prune-expired tokens to the console.php.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,5 +10,11 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->middleware(['throttle:api'])->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::prefix('v1')->middleware(['throttle:api', 'auth:sanctum'])->group(function () {
     Route::apiResource('cars', CarController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
